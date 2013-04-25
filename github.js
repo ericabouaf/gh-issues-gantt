61727bd6d6730b71620755932784627fce0a63d8
maximerety <maxime.rety@gmail.com>
2013-04-25 11:54:39 +0200

    Specifying a userAgent is now required to use github's API.

--- github.js
+++ github.js
@@ -1,4 +1,7 @@
-var request = require('request');
+var request   = require('request'),
+    // github's API requires that we use specify a userAgent
+    // (see: http://developer.github.com/v3/#user-agent-required)
+    userAgent = "gh-issues-gantt/0.0.1"; // should match package.json
 
 module.exports = function(config) {
 
@@ -39,7 +42,10 @@ module.exports = function(config) {
       console.log(theUrl);
       request.get({
          url: theUrl,
-         'auth': config.username+":"+config.password
+         'auth': config.username+":"+config.password,
+         headers: {
+            'User-Agent': userAgent
+         }
       }, function (error, response, body) {
 
          var issues = (tmpIssues ? tmpIssues : []).concat(JSON.parse(body));
@@ -70,7 +76,10 @@ module.exports = function(config) {
 
       request.get({
          url: baseUrl+"/milestones?per_page=100&status=open",
-         'auth': config.username+":"+config.password
+         'auth': config.username+":"+config.password,
+         headers: {
+            'User-Agent': userAgent
+         }
       }, function (error, response, body) {
          memo.milestones.value  = JSON.parse(body);
          memo.milestones.status = "fresh";
@@ -105,7 +114,10 @@ module.exports = function(config) {
             json: {
                "due_on": due_on
             },
-            'auth': config.username+":"+config.password
+            'auth': config.username+":"+config.password,
+            headers: {
+               'User-Agent': userAgent
+            }
          }, function (error, response, body) {
             console.log(body);
             cb(null, null);
